795b4eb1ad51f8575eba8aaea544f5425386221b
ussenuk <ukimanuka@gmail.com>
2024-02-17 11:37:11 +0300

    project skeleton

---
+++ lib/users.py
@@ -0,0 +1,22 @@
+class User():
+
+    def __init__(self, name, email, city, country):
+        self._name = name
+        self.email = email
+        self.city = city
+        self.country = country
+
+
+
+
+
+
+
+
+    def get_name(self):
+        return self._name
+    
+    name = property(get_name)
+
+if __name__ == "__main__":
+    pass
---
+++ main.py
@@ -0,0 +1,8 @@
+import ipdb
+
+from lib import members
+
+if __name__ == "__main__":
+    print("This is main")
+
+    ipdb.set_trace()
